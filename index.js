4cd678b8c5b895070f0aed7b2e3769a739eb3881
Pupchin Pavel <p.pupchin@corp.mail.ru>
2019-09-25 14:15:07 +0300

    parsing saved req

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 const optimist = require('optimist');
 const debug = require('debug')('http-mitm-proxy:bin');
+const http = require('http');
+const https = require('https');
 const fs = require('fs');
+const CircularJSON = require('circular-json');
+const js_beautify = require('js-beautify');
 const Proxy = require('./libs/main');
 
 const proxy = Proxy();
@@ -13,15 +17,43 @@ const args = optimist
     .options('host', {
         describe: 'HTTP Listen Interface.'
     })
+    .options('do-request', {
+        describe: 'Request timestamp'
+    })
     .argv;
 
 args.port = 8081;
 
-if (args.host === 'www.google.com') {
+function errorHandler(error) {
+    error ? console.log('Error on add log', error) : console.log('OK add log');
+}
+
+if (args.host === 'www.google.com' && !args['do-request']) {
     proxy.onRequest(function(ctx, callback) {
         if (ctx.clientToProxyRequest.headers.host == 'www.google.com') {
             ctx.use(Proxy.gunzip);
+            const date = Date.now();
+            fs.appendFileSync('./reqLog.txt', `\n===== Req log for google - callback =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `>Date ${date}\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', callback.toString(), (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Req log for google - callback ends =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Req log for google - ctx =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `>Date ${date}\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Req log for google - ctx ends =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
             ctx.onResponseData(function(ctx, chunk, callback) {
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - callback =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', callback.toString(), (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - callback ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - ctx =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - ctx ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - chunk =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', chunk, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for google - chunk ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
                 chunk = new Buffer(chunk.toString().replace(/<H1.*?<\/H1>/g, '<h1>Replaced google!</h1>'));
                 return callback(null, chunk);
             });
@@ -29,14 +61,30 @@ if (args.host === 'www.google.com') {
         return callback();
     });
     args.host = '';
-} else if (args.host === 'www.mail.ru') {
+} else if (args.host === 'www.mail.ru' && !args['do-request']) {
     proxy.onRequest(function(ctx, callback) {
-        fs.writeFile('./reqLog.txt', ctx, (error) => {
-            error ? console.log('Smth went wrong', error) : console.log('Request log saved');
-        })
         if (ctx.clientToProxyRequest.headers.host == 'www.mail.ru') {
             ctx.use(Proxy.gunzip);
+            fs.appendFileSync('./reqLog.txt', `\n===== Res log for mail - callback =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', callback.toString(), (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Res log for mail - callback ends =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Res log for mail - ctx =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+            fs.appendFileSync('./reqLog.txt', `\n===== Res log for mail - ctx ends =====\n`, (error) => errorHandler(error));
+            fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
             ctx.onResponseData(function(ctx, chunk, callback) {
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - callback =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', callback.toString(), (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - callback ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - ctx =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - ctx ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - chunk =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', chunk, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n===== ResData log for mail - chunk ends =====\n`, (error) => errorHandler(error));
+                fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
                 chunk = new Buffer(chunk.toString().replace(/<(H|h)1.*?<\/(H|h)1>/g, '<h1>Replaced mail!</h1>'));
                 return callback(null, chunk);
             });
@@ -44,22 +92,110 @@ if (args.host === 'www.google.com') {
         return callback();
     });
     args.host = '';
-} else {
+} else if (!args['do-request']) {
     proxy.onRequest(function(ctx, callback) {
+        fs.appendFileSync('./reqLog.txt', `\n===== Req log - callback =====\n`, (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', callback.toString(), (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', `\n===== Req log - callback ends =====\n`, (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', `\n===== Req log - ctx =====\n`, (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+        fs.appendFileSync('./reqLog.txt', `\n===== Req - ctx ends =====\n`, (error) => errorHandler(error));
+        fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
         callback();
     });
     args.host = '';
+} else if (args['do-request'] && typeof args['do-request'] === 'number') {
+    args.host = '';
+    const a = fs.readFileSync('./reqLog.txt').toString().split('\n').join('');
+    const reg = new RegExp(String(`=====>Date ${args['do-request']}(.*)>Date ${args['do-request']}=====`));
+    const re = new RegExp(String(`>Date ${args['do-request']}`));
+    const match = a.match(reg)
+
+    fs.writeFileSync('./reqToDo.txt', '', (error) => errorHandler(error));
+    fs.appendFileSync('./reqToDo.txt', match[0], (error) => errorHandler(error));
+    let b = fs.readFileSync('./reqToDo.txt').toString().split('\n').join('');
+    while (re.test(b)) {
+        b = fs.readFileSync('./reqToDo.txt').toString().split('\n').join('');
+        fs.writeFileSync('./reqToDo.txt', b.replace(`>Date ${args['do-request']}`, ''), (error) => errorHandler(error));
+    }
+    const arr = b.split('=====');
+    arr.forEach((elem, index, array) => {
+        /Req\ log\ for/.test(elem) ? array[index] = '' : console.log();
+    })
+    fs.writeFileSync('./parsed.js', arr.join('\n\n'), (error) => errorHandler(error));
+
+    const reading = fs.readFileSync('./parsed.js').toString().split('\n');
+    const res = [];
+
+    for (let i = 0; i < reading.length; i++) {
+        if (reading[i] === '') {
+            continue;
+        } else {
+            res.push(reading[i]);
+        }
+    }
+
+    res[0] = res[0].replace(/function\ \(/, 'module.exports = function callbackRequest(');
+    res[1] = 'ctx = ' + res[1];
+    res[1] = res[1].replace(/ctx\ \=\ /, 'module.exports = ');
+
+    fs.writeFileSync('./parsedCtx.js', res[1], (error) => errorHandler(error));
+    fs.writeFileSync('./parsedCallback.js', res[0], (error) => errorHandler(error));
+    fs.writeFileSync('./ctx.json', res[1], (error) => errorHandler(error));
+
+    fs.writeFileSync('./parsedCtx.js', js_beautify(fs.readFileSync('./parsedCtx.js').toString()));
+    fs.writeFileSync('./parsedCallback.js', js_beautify(fs.readFileSync('./parsedCallback.js').toString()));
+
+    const callbackRequest = require('./parsedCallback');
+    const ctx = require('./parsedCtx');
+    console.log(ctx)
+    console.log(callbackRequest.toString());
+    process.exit(0);
 }
 
 proxy.onRequestData(function(ctx, chunk, callback) {
+    const date = Date.now();
+    fs.appendFileSync('./reqLog.txt', `\n===== ReqData log - callback =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', `>Date ${date}\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', callback.toString(), (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', `\n===== ReqData log - callback ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', `\n===== ReqData log - ctx =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', `\n===== ReqData - ctx ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ReqData log - chunk =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', chunk, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ReqData - chunk ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./reqLog.txt', `>Date ${date}\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
     return callback(null, chunk);
 });
   
 proxy.onResponse(function(ctx, callback) {
+    fs.appendFileSync('./resLog.txt', `\n===== Res log - callback =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', callback.toString(), (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== Res log - callback ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== Res log - ctx =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== Res - ctx ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
     return callback(null);
 });
 
 proxy.onResponseData(function(ctx, chunk, callback) {
+    console.log('resdata')
+    fs.appendFileSync('./resLog.txt', `\n===== ResData log - callback =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `>Date ${Date.now()}\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', callback.toString(), (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ResData log - callback ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ResData log - ctx =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', CircularJSON.stringify(ctx), (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ResData - ctx ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ResData log - chunk =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', chunk, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n===== ResData - chunk ends =====\n`, (error) => errorHandler(error));
+    fs.appendFileSync('./resLog.txt', `\n=======================================\n`, (error) => errorHandler(error));
     return callback(null, chunk);
 });
 
